c32f2f04375a144d00adc185fe21fa9646117d69
ousmane~ <ousmaane07@gmail.com>
2024-01-17 01:44:52 +0000

    first commit

---
+++ src/test/java/com/groupeisi/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
